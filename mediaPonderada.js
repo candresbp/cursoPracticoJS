88fc69be7723e1ee2dd531b3de979bd9bb1e4c35
candresbp <loscarboni@hotmail.com>
2021-07-01 11:21:28 -0500

    +

---
+++ mediaPonderada.js
@@ -0,0 +1,14 @@
+const notasEstudiantes = [
+    [8, 35],
+    [10, 45],
+    [7, 20],
+];
+
+function calcularMediaPonderada(elemento1, elemento2, elemento3) {
+    for (let i = 0; i < notasEstudiantes[0]; i++) {
+        
+        notasEstudiantes[1] / 100;
+        var notaPonderada = notasEstudiantes[0] * notasEstudiantes[1];
+    }
+    
+}
--- moda.js
+++ moda.js
@@ -10,4 +10,24 @@ const lista1 = [
     2,
     2,
     1,
-]
+];
+
+const lista1Count = {};
+
+lista1.map(
+    function(elemento) {
+        if(lista1Count[elemento]){
+            lista1Count[elemento] += 1;
+        } else {
+        lista1Count[elemento] = 1;
+        }
+    }
+);
+
+const lista1Array = Object.entries(lista1Count).sort(
+    function(elementoA, elementoB) {
+        return elementoA[1] - elementoB[1];
+    }
+); //Vuelve el objeto un array
+
+const moda = lista1Array[lista1Array.length - 1];
